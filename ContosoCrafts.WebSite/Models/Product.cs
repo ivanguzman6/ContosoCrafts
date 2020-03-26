afb2aebe748127ad717a889aca40b154f3b1be2f
Ivan Guzman Brito <ivanguzman6@hotmail.com>
2020-03-25 22:50:48 -0400

    Primer Commit. Proyecto de ejercicio del curso de Youtube ASP.NET Core 101. Video 6-13

---
+++ ContosoCrafts.WebSite/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class Product
+    {
+        public String Id { get; set; }
+
+        public String Maker { get; set; }
+
+        [JsonPropertyName("img")]
+        public String Image { get; set; }
+
+        public String Url { get; set; }
+
+        public String Title { get; set; }
+
+        public String Description { get; set; }
+
+        public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+        
+    }
+}
